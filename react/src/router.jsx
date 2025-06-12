034fc18085b39c24e327bed5a91dcaa3278e40f8
Sajjad Ghanbari <a.s.ghanbari.j@gmail.com>
2025-06-12 17:00:18 +0330

    react routes modified

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,18 +1,26 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Login from './views/login.jsx';
-const router = createBrowserRouter({
-  routes: [
-    {
-      path: '/login',
-      element: <Login />,
-    },
-    {
-      path: '/user',
-      element: <User />,
-    },
-    {
-      path: '/signup',
-      element: <Signup />,
-    },
-  ],
-});
+import User from './views/User.jsx';
+import Signup from './views/Signup.jsx';
+import NotFound from './views/NotFound.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/user',
+    element: <User />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
+  },
+  {
+    path: '/*',
+    element: <NotFound />,
+  },
+]);
+
+export default router;
--- react/src/views/user.jsx
+++ react/src/views/user.jsx
@@ -1,4 +1,4 @@
-export default function User (){
+export default function User(){
     return (
         <div>
             User
