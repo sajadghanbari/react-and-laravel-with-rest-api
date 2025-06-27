a539044ae79a924d1c94589e0f7913f5f8a1f686
Sajjad Ghanbari <a.s.ghanbari.j@gmail.com>
2025-06-27 22:40:45 +0330

    signup page layout created

--- react/src/views/signup.jsx
+++ react/src/views/signup.jsx
@@ -1,7 +1,29 @@
+import { Link } from "react-router-dom";
 export default function Signup (){
+    const onSubmit = (ev) => {
+        ev.preventDefault();
+        
+        // const data = Object.fromEntries(formData);
+        // console.log(data);
+    }
     return (
-        <div>
-            Signup
+        <div className="login-signup-form animated fadeInDown">
+            <div className="form">
+                <form onSubmit={onSubmit}>
+                    <h1 className="title">
+                        Signup 
+                    </h1>
+                    <input type="text" placeholder="Full Name" />
+                    <input type="email" placeholder="Email Address" />
+                    <input type="password" placeholder="Password" />
+                    <input type="password" placeholder="Password Confirmation" />
+                    <button className="btn btn-block">Signup</button>
+                    <p className="message">
+                        Already Registered? <Link to="/login">Sign in</Link>
+                    </p>
+                </form>
+            </div>
+            
         </div>
     )
 }
