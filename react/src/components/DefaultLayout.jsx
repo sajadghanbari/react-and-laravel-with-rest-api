4f7527c72558d64f340ef57145d3181542767b28
Sajjad Ghanbari <a.s.ghanbari.j@gmail.com>
2025-06-27 22:20:30 +0330

    user layout ui created

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,4 +1,4 @@
-import { Navigate, Outlet } from "react-router-dom";
+import {Link,Navigate, Outlet } from "react-router-dom";
 import { useStateContext } from "../contexts/ContextProvider";
 
 export default function DefaultLayout() {
@@ -8,10 +8,33 @@ export default function DefaultLayout() {
   {
     return <Navigate to="/login" />
   }
+
+  const onLogout = (ev) => {
+    ev.preventDefault();
+    localStorage.removeItem('ACCESS_TOKEN');
+    window.location.reload();
+  }
+
   return (
-    <div>
-      dafdfs
-      <Outlet />
+    <div id="defaultLayout">
+      <aside>
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/users">Users</Link>
+      </aside>
+      <div className="content">
+        <header>
+          <div>
+            Header
+          </div>
+          <div>
+            {user.name}
+            <a className="btn-logout" onClick={onLogout} href="#">Logout</a>
+          </div>
+        </header>
+        <main>
+          <Outlet />
+        </main>
+      </div>
     </div>
   )
 }
--- react/src/views/user.jsx
+++ react/src/views/user.jsx
@@ -1,7 +1,7 @@
 export default function User(){
     return (
         <div>
-            Userss
+            Users
         </div>
     )
 }
