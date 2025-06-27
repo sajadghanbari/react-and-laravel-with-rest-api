92f22abb891e218ed798469a327885fc0d864551
Sajjad Ghanbari <a.s.ghanbari.j@gmail.com>
2025-06-28 00:03:33 +0330

    signup and login methods created

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
+Route::post('/signup',[AuthController::class,'signup']);
+Route::post('/login',[AuthController::class,'login']);
+Route::post('/logout',[AuthController::class,'logout']);
